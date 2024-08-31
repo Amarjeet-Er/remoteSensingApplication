8a04b84cda0bac210f1c05710a5d3eacb0ec594b
theayushkumar <ayushkumarhjp@gmail.com>
2024-08-31 16:45:51 +0530

    all changes done header

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -155,6 +155,8 @@
 		$(".menu-trigger").on('click', function () {
 			$(this).toggleClass('active');
 			$('.header-area .nav').slideToggle(200);
+			$('.header-area').toggleClass('menu-active');
+
 		});
 	}
 
@@ -176,6 +178,20 @@
 				return false;
 			}
 		}
+	});
+
+
+	// Back to top button
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 300) {
+			$('.back-to-top').fadeIn('slow');
+		} else {
+			$('.back-to-top').fadeOut('slow');
+		}
+	});
+	$('.back-to-top').click(function () {
+		$('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+		return false;
 	});
 
 	$(document).ready(function () {
